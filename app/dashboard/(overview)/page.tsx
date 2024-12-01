28529c5c64bc7973e300bc00bd7e198b7993336e
hiroki1216 <g.o.b96just.uw@icloud.com>
2024-12-01 15:16:15 +0900

    chore: Update page component to lazy load images and fetch latest invoices and card data

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,10 +4,10 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton } from '@/app/ui/skeletons';
-import {fetchLatestInvoices, fetchCardData} from '@/app/lib/data';
+import { LatestInvoicesSkeleton } from '@/app/ui/skeletons';
+import {fetchCardData} from '@/app/lib/data';
  
 export default async function Page() {
-  const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfInvoices,
     numberOfCustomers,
@@ -33,7 +33,9 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices/>
+        </Suspense>
       </div>
     </main>
   );
