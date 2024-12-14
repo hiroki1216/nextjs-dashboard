e380375a0b9d29af5dc4908d085b060c6c60f1d5
hiroki1216 <g.o.b96just.uw@icloud.com>
2024-12-14 23:12:21 +0900

    feat: Create form page with rating component integration

---
+++ app/dev/form/page.tsx
@@ -0,0 +1,12 @@
+import MyRating from "./shared/components/rating";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Form</h1>
+      <div className="mb-4">
+        <MyRating />
+      </div>
+    </main>
+  );
+}
