845320b1121a2d999adfa81092235a7bc21a0890
hiroki1216 <g.o.b96just.uw@icloud.com>
2025-04-30 08:32:55 +0900

    スクロールトップボタンコンポーネントを追加

---
+++ app/[lang]/dev/form/ui/scroll-top-button.tsx
@@ -0,0 +1,38 @@
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import Box from "@mui/material/Box";
+import Fab from "@mui/material/Fab";
+import Fade from "@mui/material/Fade";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import { MouseEvent } from "react";
+
+export default function ScrollTopButton() {
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+  });
+
+  const handleClick = (event: MouseEvent<HTMLDivElement>) => {
+    const anchor = (
+      (event.target as HTMLDivElement).ownerDocument || document
+    ).querySelector("#back-to-top-anchor");
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        block: "center",
+      });
+    }
+  };
+
+  return (
+    <Fade in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: "fixed", bottom: 16, right: 16 }}
+      >
+        <Fab size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </Box>
+    </Fade>
+  );
+}
