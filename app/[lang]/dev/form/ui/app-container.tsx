7d365cd87493c2b01aa694236569e32720024007
hiroki1216 <g.o.b96just.uw@icloud.com>
2025-04-29 22:52:57 +0900

    ナビゲーションバーを含む新しいContainerコンポーネントを追加し、AppContainerを更新

--- app/[lang]/dev/form/ui/app-container.tsx
+++ app/[lang]/dev/form/ui/app-container.tsx
@@ -1,6 +1,6 @@
 "use client";
-import NavBar from "@/app/[lang]/dev/form/ui/nav-bar";
-import { Container as MuiContainer, styled } from "@mui/material";
+import Container from "@/app/[lang]/dev/form/ui/container";
+import { styled } from "@mui/material";
 import { PropsWithChildren } from "react";
 
 const AppContent = styled("div")({
@@ -11,18 +11,10 @@ const AppContent = styled("div")({
   height: "100%",
 });
 
-const Container = styled(MuiContainer)({
-  height: "100%",
-});
-
-const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
-
 export default function AppContainer({ children }: PropsWithChildren) {
   return (
     <div lang="ja">
       <AppContent>
-        <NavBar />
-        <Offset />
         <Container>{children}</Container>
       </AppContent>
     </div>
