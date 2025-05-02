f53497e592f456e33575a9f7a0998cf7607354da
hiroki1216 <g.o.b96just.uw@icloud.com>
2025-05-02 22:30:05 +0900

    next/navigationのモックを追加し、各テスト後にモックをクリアする処理を整理

--- jest.setupAfterEnv.ts
+++ jest.setupAfterEnv.ts
@@ -1,9 +1,11 @@
 // 環境変数の設定とグローバルモックの設定の後に実行したい処理を記述する
 import "@testing-library/jest-dom/";
 
-// 各テストの前にモックをクリア
-// jest.config.tsでclearMocks: trueを指定しているので、
-// 各テストの前にモックをクリアする必要はないかも
-beforeEach(() => {
+jest.mock("next/navigation", () => ({
+  ...jest.requireActual("next/navigation"),
+  useRouter: jest.fn(),
+}));
+
+afterEach(() => {
   jest.clearAllMocks();
 });
