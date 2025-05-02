0e75083193d2d59ff59ab3b94e886c94c2ffc35d
hiroki1216 <g.o.b96just.uw@icloud.com>
2025-05-02 22:30:13 +0900

    カスタムレンダラーを追加し、LanguageProviderでラップするように設定

---
+++ __tests__/utils/custom-testing-libratry.tsx
@@ -0,0 +1,15 @@
+import { LanguageProvider } from "@/context/language-context";
+import { render, RenderOptions } from "@testing-library/react";
+import React, { ReactElement } from "react";
+
+const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
+  return <LanguageProvider initialLanguage="ja">{children}</LanguageProvider>;
+};
+
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, "wrapper">
+) => render(ui, { wrapper: AllTheProviders, ...options });
+
+export * from "@testing-library/react";
+export { customRender as render };
