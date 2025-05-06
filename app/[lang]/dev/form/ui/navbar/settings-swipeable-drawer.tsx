7dc04d25af838bd6dfdc4623c7c0d87ad89d863a
hiroki1216 <g.o.b96just.uw@icloud.com>
2025-05-06 15:39:46 +0900

    設定用のスワイプ可能なドロワーコンポーネントを追加

---
+++ app/[lang]/dev/form/ui/navbar/settings-swipeable-drawer.tsx
@@ -0,0 +1,64 @@
+"use client";
+import { DRAWER_WIDTH } from "@/app/constants/styles";
+import SettingsIcon from "@mui/icons-material/Settings";
+import Box from "@mui/material/Box";
+import SwipeableDrawer from "@mui/material/SwipeableDrawer";
+import { ReactNode, useState } from "react";
+import CustomIconButton from "./shared/components/custom-icon-button";
+
+type Anchor = "top" | "left" | "bottom" | "right";
+
+type Props = {
+  isDrawerOpen: boolean;
+  anchor: Anchor;
+  children: ReactNode;
+};
+
+export default function SwipeableTemporaryDrawer(props: Props) {
+  const { isDrawerOpen, anchor, children } = props;
+  const [isAnchorDrawerOpen, setIsAnchorDrawerOpen] = useState(isDrawerOpen);
+
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event &&
+        event.type === "keydown" &&
+        ((event as React.KeyboardEvent).key === "Tab" ||
+          (event as React.KeyboardEvent).key === "Shift")
+      ) {
+        return;
+      }
+
+      setIsAnchorDrawerOpen(open);
+    };
+
+  return (
+    <div>
+      <>
+        <CustomIconButton
+          icon={<SettingsIcon />}
+          aria-label="settings"
+          onClick={toggleDrawer(true)}
+        />
+        <SwipeableDrawer
+          anchor={anchor}
+          open={isAnchorDrawerOpen}
+          onClose={toggleDrawer(false)}
+          onOpen={toggleDrawer(true)}
+        >
+          <Box
+            sx={{
+              width:
+                anchor === "top" || anchor === "bottom" ? "auto" : DRAWER_WIDTH,
+            }}
+            role="presentation"
+            onClick={toggleDrawer(false)}
+            onKeyDown={toggleDrawer(false)}
+          >
+            {children}
+          </Box>
+        </SwipeableDrawer>
+      </>
+    </div>
+  );
+}
