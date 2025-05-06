92f38b7eda095157e85c657ec01fbfe496a6cea8
hiroki1216 <g.o.b96just.uw@icloud.com>
2025-05-06 15:10:01 +0900

    ダークモードのテーマを追加し、AppContainerコンポーネントをThemeProviderでラップ

--- app/[lang]/dev/form/ui/app-container.tsx
+++ app/[lang]/dev/form/ui/app-container.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Container from "@/app/[lang]/dev/form/ui/container";
 import { styled } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { PropsWithChildren } from "react";
 
 const AppContent = styled("div")({
@@ -11,12 +12,21 @@ const AppContent = styled("div")({
   height: "100%",
 });
 
+const theme = createTheme({
+  // ダークモードのテーマを設定（paletteが設定された場合は上書きされる）
+  colorSchemes: {
+    dark: true,
+  },
+});
+
 export default function AppContainer({ children }: PropsWithChildren) {
   return (
     <div lang="ja">
-      <AppContent>
-        <Container>{children}</Container>
-      </AppContent>
+      <ThemeProvider theme={theme}>
+        <AppContent>
+          <Container>{children}</Container>
+        </AppContent>
+      </ThemeProvider>
     </div>
   );
 }
